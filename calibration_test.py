8665de1e6a7b539100f9ef952179df3b1c218129
LuisAli22 <luisali@fi.uba.ar>
2018-11-21 17:23:59 -0300

    Refactor. Además del refactor, ahora avisa si falta mas de un dia para realizar la prueba y permite al usuario tomar una decisón si elgie continuar o no

--- calibration_test.py
+++ calibration_test.py
@@ -1,61 +1,138 @@
 #!/usr/bin/python3
 import os;
 import subprocess
-import argparse
-import sys, getopt
 import yaml
 import datetime
 import time
+from definitions import *
+from pathlib import Path
 from datetime import timedelta
 from subprocess import Popen, PIPE, signal
 from modules.torrent_manager import *
 import pdb
 
-BUILDFILE= "tix-time-client/Model/build/libs/tix-time-client.jar"
-parser = argparse.ArgumentParser()
-parser.add_argument("--user", "-u", help="Run all options")
-parser.add_argument("--password","-p");
-parser.add_argument("--port","-P");
-parser.add_argument("--logsPath","-l");
-parser.add_argument("--installation","-i");
-parser.add_argument("--torrent-file-config","-tfc");
 
-# read arguments from the command line
-args = parser.parse_args()
-
+'''
+Pre: --
+Pos: Ejecuta gradle :Model:jar. El proyecto tix-time-client debe estar al mismo
+nivel en el file system que este mismo archivo (calibration_test.py).
+Este metodo espera a que termina el build y finaliza.
+'''
+def build_tix_time_client():
+    gradle_build_process=Popen([GRADLECOMMAND, GRADLEOPTIONS],\
+                               stdout=PIPE,\
+                               stderr=PIPE,\
+                               cwd=TIX_TIME_CLIENT_PATH)
+    gradle_build_process.wait();
+'''
+PRE: Recibe el tiempo de inicio de la prueba, leido desde el archivo de
+configuracion pasado por parametro (torrent-file-config)
+POS: Devuelve el delay en segundos desde el tiempo actual de ejecucion hasta el
+tiempo de inicio de la prueba.
+Si el tiempo actual de ejecucion es mayor al tiempo indicado en el archivo de
+configuracion (torrent-file-config), entonces a la diferncia de tiempo le
+agrega un dia. Es decir, si desde el archivo de configuracion se lee start_time
+igual a 15:30 y se ejecuta calibration_test a las 15:35, se agrega un retardo de
+ejecucion de un 1 dia. Hay que esperar hasta las 15:30 del dia siguiente.
+'''
+def get_time_delay(start_value):
+    now = datetime.datetime.now();
+    start_time_string_representation =now.strftime('%Y-%m-%d')+":"+ start_value
+    start= datetime.datetime.strptime(start_time_string_representation,\
+                                      '%Y-%m-%d:%H:%M');
+    if (now > start):
+        if (continue_execution_if_time_too_long()):
+            start = start + timedelta(days=DAYSAMOUNT);
+        else:
+            sys.exit(SUCCESSRETURN);
+    return (start - now).total_seconds()
+'''
+PRE: Recibe el path donde se guardan los archivos logs
+POS: Toma el tiempo al instante de la ejecucion, crea el archivo real_start.yml
+en el path donde se guardan los archivos logs (si no existe el path, lo crea),
+y sobre este archivo escribe el valor del tiempo al momento de la ejecucion en
+formato epoch.
+'''
+def save_real_start_time_epoch_in_yml_file(logs_path):
+    real_start = dict(start_time= datetime.datetime.now().timestamp());
+    logsDirectory = str(Path.home()) +SLASH+logs_path;
+    real_start_yml_file = logsDirectory +SLASH+REALSTARTYMLFILE;
+    if not os.path.exists(logsDirectory):
+        os.makedirs(logsDirectory)
+    with open(real_start_yml_file, 'w') as outfile:
+        yaml.dump(real_start, outfile, default_flow_style=False)
+'''
+PRE: Recibe el stream correspondiente a la apertura del archivo de configuracion
+torrent_file_config para poder obtener los valores de configuracion del torrent,
+y el path donde se guardan los archivos logs para poder crear el archivo yml
+con el tiempo de inicio real de la medicion (real_start).
+POS: Obtiene el tiempo de inicio desde el archivo torrent_file_config. Calcula
+la diferencia entre este tiempo obtenido y el tiempo actual
+(al momento de ejecucion). Espera hasta llegar al tiempo de inicio. Transcurrido
+ese tiempo, guarda el tiempo de ejecucion en ese preciso momento en el
+archivo start_time.yml en la carpeta donde se guardan los logs
+'''
+def wait_until_start_time_and_save_real_start_time_epoch_in_file(config_stream,\
+                                                                 logs_path):
+    config = yaml.load(config_stream);
+    delay_seconds = get_time_delay(config[START_TIME_INDEX]);
+    time.sleep(delay_seconds);
+    save_real_start_time_epoch_in_yml_file(logs_path);
+'''
+PRE: Recibe el path del archivo de configuracion del torrent
+(torrent_file_config)
+POS: Lee desde el archivo de configuracion el valor de la velocidad maxima
+de decarga y los intervalos que indican la duracion en minutos y el porcentaje
+de velocidad a utilizar en dicho intervalo. Con estos valores se crea el
+TorrentManager y se lo ejecuta. El metodo finaliza tras la finalizacion de la
+ejecucion del TorrentManager.
+'''
+def launch_torrent_manager(torrent_file_config):
+    with open(torrent_file_config, "r") as test_configuration_file:
+        test_configuration = yaml.load(test_configuration_file);
+        max_speed = test_configuration[MAX_SPEED_INDEX];
+        intervals = test_configuration[INTERVAL_INDEX];
+        torrent_manager = TorrentManager(max_speed,intervals);
+        torrent_manager.run();
+'''
+PRE: Recibe los argumentos pasados por parametros
+POS: Toma los argumentos necesarios para ejecutar el cliente. Ejecuta el cliente
+en un subproceso. Luego lanza el TorrentManager. Tras la finalizacion del
+TorrentManager, envia SIGINT al suproceso cliente para terminarlo gracefully.
+'''
+def launch_tix_client_and_torrent_manager(args):
+    tix_client_execution_args = [JAVA_COMMAND, JAVA_OPTION, \
+                                 str(BUILDFILE),\
+                                 str(args.username),\
+                                 str(args.password),\
+                                 str(args.installation),\
+                                 str(args.port),\
+                                 str(args.logs_path)]
+    tix_time_client_process=Popen(tix_client_execution_args,\
+                                 stdout= PIPE,\
+                                 stderr=PIPE,\
+                                 cwd=CURRENT_PATH)
+    launch_torrent_manager(args.torrent_file_config)
+    tix_time_client_process.send_signal(signal.SIGINT);
+'''
+PRE: --
+POS: Leer el yaml al principio para saber cuando empizan los tests (start time).
+ Hacer un sleep de la resta entre el start time y el tiempo actual.
+ Despues calcula el start time convertido en epoch.
+ Guardar el archivo test_desription.yaml (Va a tener el start time en epoch,
+ el max speed y los intervalos). Guardarlo en la carpeta de los logs (los json))
+ Mandar a ejecutar el cliente y el torrent.
+ Mandar la senal SIGINT
+'''
 if __name__ == '__main__':
-    if ((not args.user) or (not args.password) or (not args.installation) or (not args.port) or (not args.logsPath) or (not args.torrent_file_config)):
-        print("Bad arguments. Please type -h or --help");
-        sys.exit(-1);
+    args= get_and_check_parse_arguments();
     if (not os.path.exists(BUILDFILE)):
-        gradle_build_process=Popen(['gradle', ':Model:jar'], stdout=PIPE, stderr=PIPE, cwd="tix-time-client/")
-        gradle_build_process.wait();
-    #logfile= open("runlog.txt", "w");
-    #pdb.set_trace()
-    # Leer el yaml al principio para saber cuando empizan los tests (start time)
-    # Hacer un sleep de la resta entre el start time y el tiempo actualself.
-    # Despues el start time convertido en epoc. (fijarte dia y hora para construir epoc).
-    # Guardar el archivo test_desription.yaml (Va a tener el start time en epoc, el max speed y los intervalos). Guardarlo en la carpeta de los logs (los json))
-    # Mandar a ejecutar el cliente y el torrent.
-    # Mandar la senal
-    with open("config/test.yml", 'r') as stream:
+        build_tix_time_client()
+    with open(args.torrent_file_config, 'r') as config_stream:
         try:
-            default_config = yaml.load(stream);
-            now = datetime.datetime.now();
-            start = datetime.datetime.strptime(now.strftime('%Y-%m-%d')+":"+default_config['start_time'], '%Y-%m-%d:%H:%M');
-            #Cuidado en el siguiente if. Hay precision hasta minutos. Si es el mismo segundo (now y start), la resta entre start y now puede dar negativo.
-            if ((now.hour > start.hour) or ((now.hour == start.hour) and (now.minute > start.minute)) or ((now.hour == start.hour) and (now.minute == start.minute) and (now.second > start.second))):
-                start = start + timedelta(days=1);
-            delay_seconds = (start - now).total_seconds()
-            time.sleep(delay_seconds);
-            real_start = dict(start_time= datetime.datetime.now().timestamp());
-            with open('config/real_start.yml', 'w') as outfile:
-                yaml.dump(real_start, outfile, default_flow_style=False)
+            wait_until_start_time_and_save_real_start_time_epoch_in_file(\
+                                                                config_stream,\
+                                                                args.logs_path)
+            launch_tix_client_and_torrent_manager(args)
         except yaml.YAMLError as exc:
             print(exc)
-    tix_time_client_process=Popen(['java', '-jar', str(BUILDFILE), str(args.user), str(args.password), str(args.installation), str(args.port), str(args.logsPath)], stdout= PIPE, stderr=PIPE, cwd=".")
-    with open(args.torrent_file_config, "r") as test_configuration_file:
-        test_configuration = yaml.load(test_configuration_file);
-    torrent_manager = TorrentManager(test_configuration['max_speed'], test_configuration['intervals']);
-    torrent_manager.run();
-    tix_time_client_process.send_signal(signal.SIGINT);
---
+++ definitions.py
@@ -0,0 +1,96 @@
+import argparse
+import sys, getopt
+BUILDFILE= "tix-time-client/Model/build/libs/tix-time-client.jar"
+QUESTIONTIMETOOLONG = "More than 1 day to perform test execution.\
+Continue? [y/N]"
+WRONGANSWER = "Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n"
+PROGRAMEXECUTION = 'python3.5 calibration_test'
+PROGRAMDESCRIPTION= 'System test launcher'
+PROGRAMOPTIONS = '[options]'
+LONGUSEROPTION = "--username"
+SHORTUSEROPTION = "-u"
+HELPUSEROPTION = "Tix 's username"
+REQUIREDARGGROUP= 'required arguments'
+LONGPASSOPTION = "--password"
+SHORTPASSOPTION = "-p"
+HELPPASSOPTION = "Tix 's password"
+LONGPORTOPTION = "--port"
+SHORTPORTOPTION = "-P"
+HELPPORTOPTION = "Local port to receive incoming packages"
+LONGLOGSPATHOPTION = "--logs_path"
+SHORTLOGSPATHOPTION = "-l"
+HELPLOGSPATHOPTION = "Logs files 's destination path"
+LONGINSTALLATIONOPTION = "--installation"
+SHORTINSTALLATIONOPTION = "-i"
+HELPINSTALLATIONOPTION = "User 's Tix installation name"
+LONGTORRENTCONFIGOPTION = "--torrent-file-config"
+SHORTTORRENTCONFIGOPTION = "-tfc"
+HELPTORRENTCONFIGOPTION = "Torrent configuration files 's path"
+GRADLECOMMAND= 'gradle'
+GRADLEOPTIONS= ':Model:jar'
+TIX_TIME_CLIENT_PATH= "tix-time-client/"
+DAYSAMOUNT = 1
+SUCCESSRETURN = 0
+SLASH= "/"
+REALSTARTYMLFILE = 'real_start.yml'
+START_TIME_INDEX= 'start_time'
+MAX_SPEED_INDEX= 'max_speed'
+INTERVAL_INDEX = 'intervals'
+CURRENT_PATH="."
+JAVA_COMMAND= 'java'
+JAVA_OPTION= '-jar'
+
+'''
+PRE: --
+POS: Avisa al usuario que falta mas de un dia para la ejecucion de la prueba.
+Pregunta si quiere continuar o no. Si el usuario indica que no quiere continuar,
+el metodo devuelve False. Si el usuario indica que quiere continuar, devuelve
+True. Mientras el usuario no indique una opcion valida, el metodo va a continuar
+preguntando.
+'''
+def continue_execution_if_time_too_long():
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    default="no"
+    while True:
+        sys.stdout.write(QUESTIONTIMETOOLONG)
+        choice = input().lower()
+        if choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write(WRONGANSWER)
+'''
+Pre: --
+Pos: Devuelve los argumentos de linea de comandos parseados en una estrucura.
+Si alguno de los argumentos no esta presente en la linea de comandos, va a
+mostrar un mensaje por pantalla indicando el argumento faltante y finaliza.
+Si todos los argumentos requeridos estan presenes entonces devuelve una
+estructura con los argumentos disponibles para su posterior uso.
+'''
+def get_and_check_parse_arguments():
+    parser = argparse.ArgumentParser(prog=PROGRAMEXECUTION, \
+                                     usage='%(prog)s '+ PROGRAMOPTIONS,\
+                                     description = PROGRAMDESCRIPTION)
+    required_named = parser.add_argument_group(REQUIREDARGGROUP)
+    required_named.add_argument(LONGUSEROPTION, SHORTUSEROPTION, \
+                                help=HELPUSEROPTION,\
+                                required = True)
+    required_named.add_argument(LONGPASSOPTION,SHORTPASSOPTION,\
+                                help=HELPPASSOPTION,\
+                                required = True);
+    required_named.add_argument(LONGPORTOPTION,SHORTPORTOPTION,\
+                                help=HELPPORTOPTION,\
+                                required = True);
+    required_named.add_argument(LONGLOGSPATHOPTION,SHORTLOGSPATHOPTION,\
+                                help=HELPLOGSPATHOPTION,\
+                                required = True);
+    required_named.add_argument(LONGINSTALLATIONOPTION,SHORTINSTALLATIONOPTION,\
+                                help=HELPINSTALLATIONOPTION,\
+                                required = True);
+    required_named.add_argument(LONGTORRENTCONFIGOPTION,\
+                                SHORTTORRENTCONFIGOPTION,\
+                                help=HELPTORRENTCONFIGOPTION,\
+                                required = True);
+    return parser.parse_args()
